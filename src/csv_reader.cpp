dc2a1cafa7b3aef892822418e7c4cf1cb609b393
Chanty91 <chantal.matu@strathmore.edu>
2024-09-03 23:06:36 +0300

    Changelist 1.Added csv reader

--- src/csv_reader.cpp
+++ src/csv_reader.cpp
@@ -1,3 +1,27 @@
 //
 // Created by warig on 03/09/2024.
 //
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+
+std::vector<Team> readTeamsFromCSV(const std::string & filename){
+    std::vector<Team> teams;
+    std::ifstream file(filename);
+    std::string line;
+
+    std::getline(file,line);
+    while (std::getline(file, line)) {
+        std::stringstream ss(line);
+        std::string name, localTown, stadium;
+
+        std::getline(ss, name, ',');
+        std::getline(ss, localTown, ',');
+        std::getline(ss, stadium, ',');
+
+        teams.push_back({name, localTown, stadium});
+    }
+
+    return teams;
+}
