daf85af4911e1fcc2aba1a03301f51c8cbc9bad1
Chanty91 <chantal.matu@strathmore.edu>
2024-09-03 23:20:29 +0300

    Changelist 1.Added structure to store fixture details

--- src/fixtures.h
+++ src/fixtures.h
@@ -5,4 +5,22 @@
 #ifndef SEMPROJ_FIXTURES_H
 #define SEMPROJ_FIXTURES_H
 
+
+#include <vector>
+#include <string>
+#include "teams.h"
+
+struct Fixture{
+    std::string homeTeam;
+    std::string awayTeam;
+    std::string stadium;
+    std::string localTown;
+    int leg;
+    int weekend;
+};
+
+std::vector<Fixture> generateFixtures(const std::vector<Team> & teams);
+std::vector<Fixture> scheduleFixtures(const std::vector<Fixture> & fixtures);
+void outputFixtures(const std::vector<Fixture> & fixtures);
+
 #endif //SEMPROJ_FIXTURES_H
